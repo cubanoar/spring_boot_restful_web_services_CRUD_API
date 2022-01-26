d4817f4465bf8b2d95a77310aa0d0b69d7e396df
cubanoar <dasiel.ramirez@yahoo.com>
2022-01-26 19:20:35 -0300

    Class Producto

---
+++ src/main/java/com/cubanoar/springboot/app/models/Producto.java
@@ -0,0 +1,39 @@
+package com.cubanoar.springboot.app.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table()
+public class Producto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+	private Float precio;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Float precio) {
+		this.precio = precio;
+	}
+	
+	
+}
